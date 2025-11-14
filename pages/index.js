285a1c2f581a3b8864bcff6406e12a7d8c91c8b2
shaillon <101482680+shaillon@users.noreply.github.com>
2025-11-14 18:28:16 +0000

    corrigindo a index construção

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home(params) {
   return (
-    <h1>Estamos em construção, pedimos que retorne em outro momento, obrigado.</h1>
-    <h2>"O sucesso é a soma de pequenos esforços repetidos dia após dia."</h2>
+    <h1>
+      Estamos em construção, pedimos que retorne em outro momento, obrigado. "O
+      sucesso é a soma de pequenos esforços repetidos dia após dia."
+    </h1>
   );
 }
 
