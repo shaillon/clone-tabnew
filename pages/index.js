498c94dd3954beec1c72a7be934776039b6c7fb9
shaillon <101482680+shaillon@users.noreply.github.com>
2025-11-14 18:24:36 +0000

    Atualização da pagina Construção

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home(params) {
   return (
-    <h1>"O sucesso é a soma de pequenos esforços repetidos dia após dia."</h1>
+    <h1>Estamos em construção, pedimos que retorne em outro momento, obrigado.</h1>
+    <h2>"O sucesso é a soma de pequenos esforços repetidos dia após dia."</h2>
   );
 }
 
